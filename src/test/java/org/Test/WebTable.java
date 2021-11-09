47cf31fbdae548e1a88431824c25186ce5a9766a
Vigneshramm2021 <vignesh.ram@yahoo.com>
2021-11-09 12:10:09 +0530

    new Changes

--- src/test/java/org/Test/WebTable.java
+++ src/test/java/org/Test/WebTable.java
@@ -22,6 +22,15 @@ public class WebTable {
 			WebElement webElement = findElements.get(i);
 			String text = webElement.getText();
 			System.out.println(text);
+			System.out.println(text);
+			System.out.println(text);
+			System.out.println(text);
+			System.out.println(text);
+			System.out.println(text);
+			System.out.println(text);
+			System.out.println(text);
+			System.out.println(text);
+			
 		}
 		
 	}
